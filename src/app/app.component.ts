bd2426a56de039337088e8e658644ef15d51a0c4
makopi23 <kipersonalpc@gmail.com>
2017-03-26 00:33:54 +0900

    complete MULTIPLE COMPONENTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name">
-    </div>
-  </div>
-
   <h2>My Heroes</h2>
   <ul class="heroes">
   <li *ngFor="let hero of heroes"
@@ -22,6 +13,7 @@ import { Component } from '@angular/core';
     <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
   </ul>
+  <hero-detail [hero]="selectedHero"></hero-detail>
   `,
   styles: [`
   .selected {
@@ -79,10 +71,12 @@ export class AppComponent  {
   //hero = 'Windstorm';
   selectedHero: Hero;
 
+/*
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+*/
 
   heroes = HEROES;
 
@@ -91,10 +85,7 @@ export class AppComponent  {
   }
 }
 
-export class Hero {
-  id: number;
-  name: string;
-}
+
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
